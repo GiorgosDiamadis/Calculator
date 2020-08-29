831d2e74d64974ad910846ebd3a2fd9aa74ee4a5
giorgos <diamantisgiorgos2@gmail.com>
2020-08-30 01:49:51 +0300

    Calculator completed

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -32,7 +32,7 @@ operation = {
 
 def infix_to_posfix(expression):
 
-    stack = Stack()
+    stack_num = Stack()
     postfix = ""
     i = 0
 
@@ -41,39 +41,69 @@ def infix_to_posfix(expression):
             postfix += expression[i]
             i += 1
         else:
-            if stack.is_empty():
-                stack.push(expression[i])
+            if stack_num.is_empty():
+                stack_num.push(expression[i])
                 i += 1
             else:
-                if out_pre[expression[i]] > in_pre[stack.peek()]:
-                    stack.push(expression[i])
+                if out_pre[expression[i]] > in_pre[stack_num.peek()]:
+                    stack_num.push(expression[i])
                     i += 1
-                elif out_pre[expression[i]] == in_pre[stack.peek()]:
-                    stack.pop()
+                elif out_pre[expression[i]] == in_pre[stack_num.peek()]:
+                    stack_num.pop()
                 else:
-                    postfix += stack.pop()
+                    postfix += stack_num.pop()
 
-    while not stack.is_empty() and stack.peek() != ')':
-        postfix += stack.pop()
+    while not stack_num.is_empty() and stack_num.peek() != ')':
+        postfix += stack_num.pop()
 
     return postfix
 
 
 def find_numbers(expression):
     numbers = []
-    st = ""
+    i = 0
     for char in expression:
         if char.isdigit():
-            st += char
+            i += 1
         else:
-            if len(st) > 0:
-                numbers.append(st)
-                st = ""
+            if i > 0:
+                numbers.append(i)
+                i = 0
 
-    if len(st) > 0:
-        numbers.append(st)
+    if i > 0:
+        numbers.append(i)
 
     return numbers
+
+
+def evaluate(posfix, numbers):
+    stack_num = Stack()
+    stack_oper = Stack()
+    k = 0
+    i = 0
+    num = ""
+    result = 0
+    while i < len(posfix):
+        if not in_pre.__contains__(posfix[i]):
+            if k == 0:
+                for j in range(numbers[k]):
+                    num += posfix[j]
+            else:
+                for j in range(i, i + numbers[k]):
+                    num += posfix[j]
+            stack_num.push(float(num))
+            i += numbers[k]
+            k += 1
+            num = ""
+        else:
+            x2 = stack_num.pop()
+            x1 = stack_num.pop()
+
+            result = operation[posfix[i]](x1, x2)
+
+            stack_num.push(result)
+            i += 1
+    return result
 
 
 def main():
@@ -81,8 +111,8 @@ def main():
         expression = input("Enter expression\n")
         posfix = infix_to_posfix(expression)
         numbers = find_numbers(expression)
-        print(numbers)
-        print(posfix)
+        res = evaluate(posfix, numbers)
+        print(res)
 
 
 if __name__ == "__main__":

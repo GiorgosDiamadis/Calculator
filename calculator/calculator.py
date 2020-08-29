9fd12361c32ab75877aa45707908d68801243b68
giorgos <diamantisgiorgos2@gmail.com>
2020-08-29 17:41:41 +0300

    building calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,6 @@
 from stack import Stack
 
-precedence_in = {
+in_pre = {
     '+': 2,
     '-': 2,
     '*': 4,
@@ -10,7 +10,7 @@ precedence_in = {
     ')': 0
 }
 
-precedence_out = {
+out_pre = {
     '+': 1,
     '-': 1,
     '*': 3,
@@ -25,7 +25,8 @@ operation = {
     '+': lambda a, b: a+b,
     '-': lambda a, b: a-b,
     '*': lambda a, b: a*b,
-    '/': lambda a, b: a/b
+    '/': lambda a, b: a/b,
+    '^': lambda a, b: a**b
 }
 
 
@@ -36,7 +37,7 @@ def infix_to_posfix(expression):
     i = 0
 
     while i < len(expression):
-        if not precedence_in.__contains__(expression[i]):
+        if not in_pre.__contains__(expression[i]):
             postfix += expression[i]
             i += 1
         else:
@@ -44,24 +45,44 @@ def infix_to_posfix(expression):
                 stack.push(expression[i])
                 i += 1
             else:
-                if precedence_out[expression[i]] > precedence_in[stack.peek()]:
+                if out_pre[expression[i]] > in_pre[stack.peek()]:
                     stack.push(expression[i])
                     i += 1
-                elif precedence_out[expression[i]] == precedence_in[stack.peek()]:
+                elif out_pre[expression[i]] == in_pre[stack.peek()]:
                     stack.pop()
                 else:
                     postfix += stack.pop()
 
     while not stack.is_empty() and stack.peek() != ')':
         postfix += stack.pop()
+
     return postfix
+
+
+def find_numbers(expression):
+    numbers = []
+    st = ""
+    for char in expression:
+        if char.isdigit():
+            st += char
+        else:
+            if len(st) > 0:
+                numbers.append(st)
+                st = ""
+
+    if len(st) > 0:
+        numbers.append(st)
+
+    return numbers
 
 
 def main():
     while 1:
         expression = input("Enter expression\n")
-        expression = infix_to_posfix(expression)
-        print(expression)
+        posfix = infix_to_posfix(expression)
+        numbers = find_numbers(expression)
+        print(numbers)
+        print(posfix)
 
 
 if __name__ == "__main__":
